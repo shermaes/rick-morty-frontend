22cda839892bf920733d9024e08ccbe479bfc6d0
Sherilyn Maestre <you@example.com>
2022-08-04 12:03:04 -0500

    initialized the project, get all characters request is working

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
+import imageRickMorty from './img/rick-morty.png'
 import './App.css';
 
 function App() {
+
+  const reqApi = async () =>{
+    // esta es async porque debe esperar la respuesta de la api
+    //usaremos esta funcion para buscar los personajes
+    const api =  await fetch('https://rickandmortyapi.com/api/character');
+    // al usar async tambien debo usar el await
+    const characterApi = await api.json();
+    //Al hacer esto obtendremos informacion que podremos utilizar en nuestra pagina
+    console.log(characterApi);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="title">Rick & Morty</h1>
+        <img src={imageRickMorty} alt="Rick & Morty" className="image-home"/>
+        <button onClick={reqApi} className="btn-search">Buscar Personajes</button>
       </header>
     </div>
   );
