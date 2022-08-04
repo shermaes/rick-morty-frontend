248d893158990168ed54d343664d4e5655221021
Sherilyn Maestre <you@example.com>
2022-08-04 12:33:02 -0500

    saving the comments on how to use props

--- src/App.js
+++ src/App.js
@@ -4,28 +4,34 @@ import { useState } from 'react';
 import Characters from './components/Characters';
 
 function App() {
-  //paso 3) usaremos un useState para crea un nuevo estado para renderizar todos los datos
   const [characters, setCharacters]=useState(null);
+  // 1)los props es el canal por donde los componentes se van a comunicar, que vamos a pasar como props? 
+  //los characters
 
-  //Paso 2 realizar el request de todos los caracteres para obtener los datos necesarios
   const reqApi = async () =>{
-    // esta es async porque debe esperar la respuesta de la api
-    //usaremos esta funcion para buscar los personajes
     const api =  await fetch('https://rickandmortyapi.com/api/character');
-    // al usar async tambien debo usar el await
     const characterApi = await api.json();
-    //Al hacer esto obtendremos informacion que podremos utilizar en nuestra pagina
     setCharacters(characterApi.results)
-    //el .results viene de la misma estructura que pedi en el request
   };
-  //Paso 1) Pintar el home
+  
+  //2)al componente Characters le vamos a pasar los characters como parametro, el paso 3 se encuentra en el archivo Characters.js
+  
+  //paso 4) Reorganizamos el return, tenemos unas llaves con el siguiente if:
+  // si characters existe nos va a mostrar el componente <Characters/> y si no nos mostrara la imagen y el boton del home
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="title">Rick & Morty</h1>
-        <Characters characters={characters}/>
-        <img src={imageRickMorty} alt="Rick & Morty" className="image-home"/>
+        {characters ? (
+        <Characters characters={characters}/>  
+        ) : (
+          <>
+           <img src={imageRickMorty} alt="Rick & Morty" className="image-home"/>
         <button onClick={reqApi} className="btn-search">Buscar Personajes</button>
+          </>
+        )}
+        
+        
       </header>
     </div>
   );
--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,5 +1,7 @@
 export default function Characters(props) {
+    //paso 3) aca le estamos diceindo al componente que vamos a recibir props
     console.log(props);
+    //paso 4) al imprimir los props que recibimos vemos que estamos recibiendo los datos de los characters
   return (
     <h1>Hola desde characters js</h1>
   )
