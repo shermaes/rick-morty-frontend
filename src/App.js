e5e61a8ad42102df8dc4e5c9f1693a469d72bca6
Sherilyn Maestre <you@example.com>
2022-08-04 12:15:31 -0500

    saving the comments on how to get the information from the API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import imageRickMorty from './img/rick-morty.png'
 import './App.css';
+import { useState } from 'react';
+import Characters from './components/Characters';
 
 function App() {
+  //paso 3) usaremos un useState para crea un nuevo estado para renderizar todos los datos
+  const [characters, setCharacters]=useState(null);
 
+  //Paso 2 realizar el request de todos los caracteres para obtener los datos necesarios
   const reqApi = async () =>{
     // esta es async porque debe esperar la respuesta de la api
     //usaremos esta funcion para buscar los personajes
@@ -10,12 +15,15 @@ function App() {
     // al usar async tambien debo usar el await
     const characterApi = await api.json();
     //Al hacer esto obtendremos informacion que podremos utilizar en nuestra pagina
-    console.log(characterApi);
+    setCharacters(characterApi.results)
+    //el .results viene de la misma estructura que pedi en el request
   };
+  //Paso 1) Pintar el home
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="title">Rick & Morty</h1>
+        <Characters characters={characters}/>
         <img src={imageRickMorty} alt="Rick & Morty" className="image-home"/>
         <button onClick={reqApi} className="btn-search">Buscar Personajes</button>
       </header>
---
+++ src/components/Characters.js
@@ -0,0 +1,6 @@
+export default function Characters(props) {
+    console.log(props);
+  return (
+    <h1>Hola desde characters js</h1>
+  )
+}
